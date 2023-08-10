98cca8fbb1fbb107e3c7f72eb50acbae4b1990df
Eduardo Ramos <eduardorhamos@outlook.com>
2023-08-10 16:28:51 -0300

    resolving merge

--- blueprints/volunteer_blueprint.py
+++ blueprints/volunteer_blueprint.py
@@ -5,16 +5,9 @@ from dtos.volunteer_dto import VolunteerDto
 from services.volunteer_service import VolunteerService
 
 
-<<<<<<< HEAD
-volunteer_blueprint = Blueprint("volunteer_controller", __name__)
-
-
-# REVER NECESSIDADE DO UUID
-=======
 volunteer_blueprint = Blueprint("volunteer_print", __name__)
 
 
->>>>>>> master
 @volunteer_blueprint.route("/voluntario", methods=["POST"])
 def post_volunteer():
     volunteer_json = request.get_json()
@@ -25,16 +18,6 @@ def post_volunteer():
         volunteer_service = VolunteerService()
         volunteer_dict = volunteer_service.save(volunteer_dto)
 
-<<<<<<< HEAD
-        return volunteer_dict, 201, {"teste": "123"}
-    except ValidationError as e:
-        print(e.args)
-
-    except Exception as e:
-        print(e.args)
-
-        return {"message": "Ocorreu um erro!"}, 500
-=======
         return volunteer_dict, 201, {"teste_header_response": "123"}
     except ValidationError as e:
         print("BODY REQUEST INVÁLIDO!")
@@ -43,7 +26,6 @@ def post_volunteer():
         print(e.args)
 
         return {"message": "Ocorreu um erro."}, 500
->>>>>>> master
 
 
 @volunteer_blueprint.route("/voluntario", methods=["GET"])
@@ -53,19 +35,11 @@ def get_volunteers():
 
         volunteer_dict_list, status_code = volunteer_service.get_all()
 
-<<<<<<< HEAD
-        return volunteer_dict_list, status_code
-    except Exception as e:
-        print(e.args)
-
-        return {"message": "Ocorreu um erro!"}, 500
-=======
         return volunteer_dict_list, status_code, {"teste_header_response": "123"}
     except Exception as e:
         print(e.args)
 
         return {"message": "Ocorreu um erro."}, 500
->>>>>>> master
 
 
 @volunteer_blueprint.route("/voluntario/<id>", methods=["GET"])
@@ -75,19 +49,11 @@ def get_volunteer(id):
 
         volunteer_dict, status_code = volunteer_service.get_one_by_id(id)
 
-<<<<<<< HEAD
-        return volunteer_dict, status_code
-    except Exception as e:
-        print(e.args)
-
-        return {"message": "Ocorreu um erro!"}, 500
-=======
         return volunteer_dict, status_code, {"teste_header_response": "123"}
     except Exception as e:
         print(e.args)
 
         return {"message": "Ocorreu um erro."}, 500
->>>>>>> master
 
 
 @volunteer_blueprint.route("/voluntario/<id>", methods=["PUT"])
@@ -95,16 +61,8 @@ def put_volunteer(id):
     try:
         volunteer_service = VolunteerService()
 
-<<<<<<< HEAD
-        volunteer_service.update(id)
-    except Exception as e:
-        print(e.args)
-
-        return{"message": "Ocorreu um erro!"}, 500
-=======
         volunteer_service.update()
     except Exception as e:
         print(e.args)
 
         return {"message": "Ocorreu um erro."}, 500
->>>>>>> master
